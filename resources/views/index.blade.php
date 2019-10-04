7502ad9e516e6951646eea5cefba9a3d9c71b79e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-10-04 02:18:29 +0200

    Begin working on navigation bar styling
    The mobile menu interaction isn't been implemented yet.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,22 +8,8 @@
   <h2>Welcome</h2>
 
   @if (session('status'))
-    {{ session('status') }}
+  {{ session('status') }}
   @endif
-
-@if (Auth::check())
-  {{ Auth::user()->name }}
-  <form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit">{{ __('Log out') }}</button>
-  </form>
-@else
-  <a href="{{ route('register') }}">{{ __('Sign up') }}</a>
-  <a href="{{ route('login') }}">{{ __('Log in') }}</a>
-@endif
-
-
-  <hr>
 
   <section>
     <form method="GET" action="{{ route('asset.index') }}">
@@ -32,9 +18,9 @@
         <option value="" @if (Request::get('category') === null) selected @endif>Any</option>
 
         @foreach (range(0, $assetClass::CATEGORY_MAX - 1) as $categoryId)
-          <option value="{{ $categoryId }}" @if (intval(Request::get('category')) === $categoryId) selected @endif>
+        <option value="{{ $categoryId }}" @if (intval(Request::get('category')) === $categoryId) selected @endif>
           {{ $assetClass::getCategoryName($categoryId) }}
-          </option>
+        </option>
         @endforeach
       </select>
 
@@ -48,24 +34,24 @@
 
   <section class="flex flex-wrap -mx-2">
     @foreach ($assets as $asset)
-      <div class="w-full lg:w-1/2 px-2 my-2">
-        <a href="{{ route('asset.show', ['asset' => $asset ]) }}">
-          <article class="flex bg-white rounded shadow p-3">
-            <div class="flex-shrink-0 self-center">
-              <img class="w-16 h-16">
+    <div class="w-full lg:w-1/2 px-2 my-2">
+      <a href="{{ route('asset.show', ['asset' => $asset ]) }}">
+        <article class="flex bg-white rounded shadow p-3">
+          <div class="flex-shrink-0 self-center">
+            <img class="w-16 h-16">
+          </div>
+          <div class="ml-6 pt-1">
+            <div class="title leading-relaxed">{{ $asset->title }}</div>
+            <div class="author text-gray-600 text-sm">{{ $asset->author->name }}</div>
+            <div class="text-sm -ml-px mt-2">
+              <span class="m-1 px-3 py-1 bg-gray-200 rounded-full">{{ $asset->category }}</span>
+              <span class="m-1 px-3 py-1 bg-gray-200 rounded-full">{{ $asset->godot_version }}</span>
+              <span class="m-1 px-3 py-1 bg-gray-200 rounded-full">{{ $asset->support_level }}</span>
             </div>
-            <div class="ml-6 pt-1">
-              <div class="title leading-relaxed">{{ $asset->title }}</div>
-              <div class="author text-gray-600 text-sm">{{ $asset->author->name }}</div>
-              <div class="text-sm -ml-px mt-2">
-                <span class="m-1 px-3 py-1 bg-gray-200 rounded-full">{{ $asset->category }}</span>
-                <span class="m-1 px-3 py-1 bg-gray-200 rounded-full">{{ $asset->godot_version }}</span>
-                <span class="m-1 px-3 py-1 bg-gray-200 rounded-full">{{ $asset->support_level }}</span>
-              </div>
-            </div>
-          </article>
-        </a>
-      </div>
+          </div>
+        </article>
+      </a>
+    </div>
     @endforeach
   </section>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,57 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="theme-color" content="#3d8fcc">
-    <title>@yield('title') - Godot Asset Library</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <script defer src="{{ mix('js/manifest.js') }}"></script>
-    <script defer src="{{ mix('js/vendor.js') }}"></script>
-    <script defer src="{{ mix('js/app.js') }}"></script>
-  </head>
-  <body data-barba="wrapper">
-    <header>
-      <nav>
-        <a href="{{ route('asset.index') }}">Godot Asset Library</a>
-      </nav>
-    </header>
-    <main data-barba="container">
-      @yield('content')
-    </main>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="theme-color" content="#3d8fcc">
+  <title>@yield('title') - Godot Asset Library</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <script defer src="{{ mix('js/manifest.js') }}"></script>
+  <script defer src="{{ mix('js/vendor.js') }}"></script>
+  <script defer src="{{ mix('js/app.js') }}"></script>
+</head>
+<body data-barba="wrapper">
+  <header>
+    <nav class="shadow bg-white p-2">
+      <div class="container flex flex-wrap justify-between">
+
+        <div class="flex items-center mr-6">
+          <a href="{{ route('asset.index') }}" class="font-medium text-lg">Godot Asset Library</a>
+        </div>
+
+        <div class="block lg:hidden">
+          <button class="flex items-center px-3 py-2">
+            <svg class="fill-current h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+            </svg>
+          </button>
+        </div>
+
+        <div class="w-full lg:flex lg:items-center lg:w-auto">
+          @if (Auth::check())
+          {{ Auth::user()->name }}
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit">{{ __('Log out') }}</button>
+          </form>
+          @else
+          <a href="{{ route('register') }}" class="navbar-link">
+            {{ __('Sign up') }}
+          </a>
+          <a href="{{ route('login') }}" class="navbar-link">
+            {{ __('Log in') }}
+          </a>
+          @endif
+        </div>
+
+      </div>
+    </nav>
+  </header>
+
+  <main data-barba="container">
+    @yield('content')
+  </main>
+</body>
 </html>
