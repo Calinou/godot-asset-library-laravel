65d00b83bde053d171444f980b70073a833564f2
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-10-09 19:46:08 +0200

    Add a responsive favicon
    Generated using RealFaviconGenerator.net.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,17 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="apple-mobile-web-app-title" content="Godot Asset Library">
+  <meta name="application-name" content="Godot Asset Library">
   <meta name="theme-color" content="#3d8fcc">
+  <meta name="msapplication-TileColor" content="#ffffff">
   <title>@yield('title') - Godot Asset Library</title>
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('/site.webmanifest') }}">
+  <link rel="mask-icon" href="{{ asset('/safari-pinned-tab.svg') }}" color="#3d8fcc">
   <script defer src="{{ mix('js/manifest.js') }}"></script>
   <script defer src="{{ mix('js/vendor.js') }}"></script>
   <script defer src="{{ mix('js/app.js') }}"></script>
