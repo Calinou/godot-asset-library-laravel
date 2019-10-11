5c4d46dc495f23d66f7499fe9c400e2a27f944e4
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-10-11 23:01:19 +0200

    Implement theming for the password reset forms

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,26 +1,40 @@
 @extends('layouts.app')
 
+@section('title', __('Forgot your password?'))
+
 @section('content')
-@if (session('status'))
-  <div role="alert">
-    {{ session('status') }}
-  </div>
-@endif
+<div class="container">
+  <form method="POST" action="{{ route('password.email') }}">
+    @csrf
 
-<form method="POST" action="{{ route('password.email') }}">
-  @csrf
+    <div class="text-center text-xl font-medium">
+      {{ __('Forgot your password?') }}
+    </div>
 
-  <label for="email">{{ __('Email address') }}</label>
-  <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <section class="w-full max-w-xs mx-auto mt-8 bg-white rounded shadow p-4">
+      <div class="mb-8">
+        <label for="email" class="form-label">{{ __('Email address') }}</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value="{{ old('email') }}"
+          required
+          autocomplete="email"
+          autofocus
+          class="form-input-text"
+        >
+        @error('email')
+        <div role="alert" class="form-error">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
 
-  @error('email')
-    <span role="alert">
-      <strong>{{ $message }}</strong>
-    </span>
-  @enderror
-
-  <button type="submit">
-    {{ __('Send Password Reset Link') }}
-  </button>
-</form>
+      <button class="button button-primary w-full" type="submit">
+        {{ __('Send password reset link') }}
+      </button>
+    </section>
+  </form>
+</div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,33 +1,69 @@
 @extends('layouts.app')
 
+@section('title', __('Choose a new password'))
+
 @section('content')
-<form method="POST" action="{{ route('password.update') }}">
-  @csrf
-  <input type="hidden" name="token" value="{{ $token }}">
+<div class="container">
+  <form method="POST" action="{{ route('password.update') }}">
+    @csrf
+    <input type="hidden" name="token" value="{{ $token }}">
 
-  <label for="email">{{ __('Email address') }}</label>
-  <input id="email" type="email" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+    <div class="text-center text-xl font-medium">
+      {{ __('Choose a new password') }}
+    </div>
 
-  @error('email')
-    <span role="alert">
-      <strong>{{ $message }}</strong>
-    </span>
-  @enderror
+    <section class="w-full max-w-xs mx-auto mt-8 bg-white rounded shadow p-4">
+      <div class="mb-6">
+        <label for="email" class="form-label">{{ __('Email address') }}</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value="{{ old('email') }}"
+          required
+          autocomplete="email"
+          class="form-input-text"
+        >
+        @error('email')
+        <div role="alert" class="form-error">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
 
-  <label for="password">{{ __('Password') }}</label>
-  <input id="password" type="password" name="password" required autocomplete="new-password">
+      <div class="mb-6">
+        <label for="password" class="form-label">{{ __('Password') }}</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          required
+          autocomplete="current-password"
+          class="form-input-text"
+        >
+        @error('password')
+        <div role="alert" class="form-error">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
 
-  @error('password')
-    <span role="alert">
-      <strong>{{ $message }}</strong>
-    </span>
-  @enderror
+      <div class="mb-8">
+        <label for="password-confirm" class="form-label">{{ __('Confirm password') }}</label>
+        <input
+          id="password-confirm"
+          type="password"
+          name="password_confirmation"
+          required
+          autocomplete="new-password"
+          class="form-input-text"
+        >
+      </div>
 
-  <label for="password-confirm">{{ __('Confirm Password') }}</label>
-  <input id="password-confirm" type="password" name="password_confirmation" required autocomplete="new-password">
-
-  <button type="submit">
-    {{ __('Reset Password') }}
-  </button>
-</form>
+      <button class="button button-primary w-full" type="submit">
+        {{ __('Reset password') }}
+      </button>
+    </section>
+  </form>
+</div>
 @endsection
