33d82054e4d0d2662ba11ef257c6b2ed05192ba9
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-10-12 23:20:19 +0200

    Add tests for the authentication views

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    public function testAuthLoginView(): void
+    {
+        $response = $this->get('/login');
+        $response->assertOk()->assertViewIs('auth.login');
+    }
+
+    public function testAuthRegisterView(): void
+    {
+        $response = $this->get('/register');
+        $response->assertOk()->assertViewIs('auth.register');
+    }
+
+    public function testAuthForgotPasswordView(): void
+    {
+        $response = $this->get('/password/reset');
+        $response->assertOk()->assertViewIs('auth.passwords.email');
+    }
+
+    public function testAuthPasswordResetView(): void
+    {
+        // The password reset view is displayed if any parameter is passed to the route
+        // (even if it's invalid)
+        $response = $this->get('/password/reset/something');
+        $response->assertOk()->assertViewIs('auth.passwords.reset');
+    }
+}
