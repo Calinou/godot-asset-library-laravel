9a5ebe1bbace955f713d173c41d8c20d07533725
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-10-24 20:36:49 +0200

    Add `maxlength` HTML validation to the asset submission form
    - Increase the maximum blurb length to 60 characters.
    - Limit the version string to 50 characters.
    - Limit URLs to 2,000 characters, just in case.

--- app/Http/Requests/SubmitAsset.php
+++ app/Http/Requests/SubmitAsset.php
@@ -26,31 +26,25 @@ class SubmitAsset extends FormRequest
     {
         return [
             'title' => 'required|string|max:50',
-            'blurb' => 'nullable|string|max:50',
+            'blurb' => 'nullable|string|max:60',
             'description' => 'required|string|max:10000',
             'category_id' => 'required|integer|gte:0|lt:'.Asset::CATEGORY_MAX,
-            'cost' => [
-                'required',
-                Rule::in(array_keys(Asset::LICENSES)),
-            ],
-            'browse_url' => 'required|url',
-            'issues_url' => 'nullable|url',
-            'icon_url' => 'nullable|url',
+            'cost' => ['required', Rule::in(array_keys(Asset::LICENSES))],
+            'browse_url' => 'required|url|max:2000',
+            'issues_url' => 'nullable|url|max:2000',
+            'icon_url' => 'nullable|url|max:2000',
 
             // An asset must have at least one version registered
             'versions' => 'required|array|min:1',
-            'versions.*.version_string' => 'required|string',
-            'versions.*.godot_version' => [
-                'required',
-                Rule::in(Asset::GODOT_VERSIONS),
-            ],
+            'versions.*.version_string' => 'required|string|max:50',
+            'versions.*.godot_version' => ['required', Rule::in(Asset::GODOT_VERSIONS)],
             'versions.*.download_url' => 'nullable|url',
 
             // Asset previews are optional, though (even if recommended)
             'previews' => 'nullable|array',
             'previews.*.type_id' => 'required|integer|gte:0|lt:'.AssetPreview::TYPE_MAX,
-            'previews.*.link' => 'required|url',
-            'previews.*.download_url' => 'nullable|url',
+            'previews.*.link' => 'required|url|max:2000',
+            'previews.*.download_url' => 'nullable|url|max:2000',
         ];
     }
 }
