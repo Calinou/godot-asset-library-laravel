f74cf9a521f272f854d03e990b1129486a395090
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-10-27 20:12:47 +0100

    Make `testAdminCreate()` also check the user's administrator status

--- app/User.php
+++ app/User.php
@@ -43,6 +43,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_admin' => 'boolean',
     ];
 
     /**
--- tests/Feature/AdminCreateTest.php
+++ tests/Feature/AdminCreateTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -12,11 +13,15 @@ class AdminCreateTest extends TestCase
 
     /**
      * Test the administrator creation command.
+     * This also checks whether the newly created user is actually
+     * marked as an administrator after being created.
      */
     public function testAdminCreate()
     {
         $exitCode = Artisan::call('admin:create admin@example.com admin Administrator');
+        $user = User::where(['email' => 'admin@example.com'])->first();
 
         $this->assertEquals(0, $exitCode);
+        $this->assertTrue($user->is_admin);
     }
 }
