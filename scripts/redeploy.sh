d83b385b9b18a70d9562422596e9a5da205356e0
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-11-08 17:46:13 +0100

    Add a redeployment script for demo/staging instances

---
+++ scripts/redeploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# This script is intended to be run from a webhook to redeploy a demo
+# or staging instance.
+# NOTE: All data will be removed when redeploying the instance!
+
+set -euo pipefail
+IFS=$'\n\t'
+
+export DIR
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$DIR/../"
+
+# Fetch updates from the Git repository
+git stash push
+git pull --rebase
+# We may not always have a stash available, so allow failure
+git stash pop || true
+
+# Install dependencies
+composer install
+yarn install --frozen-lockfile
+
+# Build site files
+yarn run prod
+yarn run doc
+
+# Run database migrations, seed test data and create a test user
+php artisan migrate:refresh --seed
+php artisan admin:create "demo@example.com" "demo" "Demo User"
