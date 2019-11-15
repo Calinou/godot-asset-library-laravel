79dcd8adb363d3d7095189f7ec90bcd0a979ff8a
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-11-15 19:50:52 +0100

    Automatically use the `APP_NAME` as the email's "from" field
    - Add `MAIL_FROM_ADDRESS` to `.env.example` as it needs to be changed
      when deploying the application.

--- config/mail.php
+++ config/mail.php
@@ -59,7 +59,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('APP_NAME', 'Godot Asset Library'),
     ],
 
     /*
