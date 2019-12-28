ffb94a6f0f1de461ddc67509f62b52ff82fbef07
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-12-28 23:54:42 +0100

    Improve the administration panel user list display
    - Display the number of assets and reviews posted by each user.
      This makes reviewing users for spam easier.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,12 +9,13 @@
   </h2>
 
   <div class="mt-8">
-    <table class="w-full shadow rounded text-sm">
+    <table class="w-full xl:w-3/4 mx-auto shadow rounded text-sm">
       <thead>
         <tr class="font-bold">
-          <td class="bg-white border dark:bg-gray-800 px-3 py-1 text-right">{{ __('Name') }}</td>
-          <td class="bg-white border dark:bg-gray-800 px-3 py-1">{{ __('Email address') }}</td>
-          <td class="bg-white border dark:bg-gray-800 px-3 py-1">{{ __('Registered') }}</td>
+          <td class="bg-white border dark:bg-gray-800 px-3 py-1 text-right">{{ __('Registered') }}</td>
+          <td class="bg-white border dark:bg-gray-800 px-3 py-1">{{ __('Name') }}</td>
+          <td class="bg-white border dark:bg-gray-800 px-3 py-1">{{ __('Assets') }}</td>
+          <td class="bg-white border dark:bg-gray-800 px-3 py-1">{{ __('Reviews') }}</td>
           <td class="bg-white border dark:bg-gray-800 px-3 py-1">{{ __('Actions') }}</td>
         </tr>
       <tbody>
@@ -30,6 +31,10 @@
         @endphp
         <tr class="{{ $rowClasses }}">
           <td class="border px-3 py-1 text-right">
+            @include('includes/date-relative', ['date' => \Carbon\Carbon::parse($user->created_at)])
+          </td>
+
+          <td class="border px-3 py-1">
             <div
               @if ($user->is_admin)
               aria-label="{{ __('Administrator') }}"
@@ -38,6 +43,8 @@
               class="py-1"
               @endif
             >
+
+              <a class="link" href="mailto:{{ $user->email }}"><span class="fa fa-envelope fa-fw mr-1 opacity-75"></span></a>
               @if ($user->is_admin)
               <span class="fa fa-shield fa-fw mr-1 text-yellow-600"></span>
               @endif
@@ -47,14 +54,12 @@
             </div>
           </td>
 
-          <td class="border px-3 py-1">
-            <a class="link" href="mailto:{{ $user->email }}">
-              {{ $user->email }}
-            </a>
+          <td class="border px-3 py-1 @if ($user->assets->count() == 0) opacity-50 @endif">
+            {{ $user->assets->count() }}
           </td>
 
-          <td class="border px-3 py-1">
-            @include('includes/date-relative', ['date' => \Carbon\Carbon::parse($user->created_at)])
+          <td class="border px-3 py-1 @if ($user->assetReviews->count() == 0) opacity-50 @endif">
+            {{ $user->assetReviews->count() }}
           </td>
 
           <td class="border px-3 py-1">
