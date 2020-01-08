293ce142a343bb286b5a385345c5da1231b36b8d
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-01-08 16:49:50 +0100

    Base email addresses on the username in the user factory
    Now that usernames are used to identify users, this makes
    email addresses easy to guess for development purposes again.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,16 +8,16 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
-    // Use the same name for email and password to make email addresses
+    // Use the same name for username and email address to make email addresses
     // easily guessable. This makes it easier to log in as a given user
     // for development purposes.
     $firstName = $faker->firstName;
-    $lastName = $faker->lastName;
+    $username = $firstName.$faker->numberBetween(0, 9);
 
     return [
-        'username' => $firstName.$faker->numberBetween(0, 9),
-        'full_name' => "$firstName $lastName",
-        'email' => Str::lower("$firstName.$lastName@example.com"),
+        'username' => $username,
+        'full_name' => "$firstName $faker->lastName",
+        'email' => Str::lower("$username@example.com"),
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
