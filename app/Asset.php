f242a65e7772fcc3b4bd2a15f549b446f2399c89
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-10-17 16:58:51 +0200

    Update list of available licenses to match the old asset library

--- app/Asset.php
+++ app/Asset.php
@@ -117,9 +117,12 @@ class Asset extends Model
      * Should be kept in alphabetical order.
      */
     public const LICENSES = [
+        'AGPL-3.0-only' => 'AGPLv3 only',
+        'AGPL-3.0-or-later' => 'AGPLv3 or later',
         'Apache-2.0' => 'Apache 2',
         'BSD-2-Clause' => 'BSD 2-Clause',
         'BSD-3-Clause' => 'BSD 3-Clause',
+        'BSL-1.0' => 'Boost Software License',
         'CC0-1.0' => 'CC0 1.0 Universal',
         'CC-BY-3.0' => 'CC BY 3.0 Unported',
         'CC-BY-4.0' => 'CC BY 4.0 International',
@@ -135,6 +138,7 @@ class Asset extends Model
         'GPL-3.0-or-later' => 'GPLv3 or later',
         'MIT' => 'MIT',
         'MPL-2.0' => 'MPLv2',
+        'Unlicense' => 'The Unlicense License',
     ];
 
     /**
